bd7a333445279534a0d1a43151fdba613d4a8c7e
Justus Kelsch <33078472+JustusKelsch@users.noreply.github.com>
2024-10-16 17:01:41 -0600

    Added caching for locations that have already been retrieved.

--- main.go
+++ main.go
@@ -7,10 +7,10 @@ import (
 )
 
 func main() {
-	pokeClient := pokeapi.NewClient(5 * time.Second)
+	pokeClient := pokeapi.NewClient(5*time.Second, time.Minute*5)
 	config := &config{
 		pokeapiClient: pokeClient,
 	}
-	
+
 	startRepl(config)
 }
