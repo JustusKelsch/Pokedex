027351d748e49cc4dae47c72d2eafd9bf00e9958
Justus <33078472+JustusKelsch@users.noreply.github.com>
2024-10-15 23:31:35 -0600

    Added the ability to ask for help(prints how to use and the available commands) and exit

---
+++ command_help.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func commandHelp() error {
+
+	fmt.Println()
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+
+	commands := getCommands()
+	for _, command := range commands {
+		fmt.Printf("%v: %v\n", command.name, command.description)
+	}
+	fmt.Println()
+	
+	return nil
+}
